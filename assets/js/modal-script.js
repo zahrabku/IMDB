9fe93681b3bcbbaaf8186573d3ca9e81f2e64670
zahrabku <zahrabku>
2021-07-26 11:02:30 +0430

    fix bug

--- assets/js/modal-script.js
+++ assets/js/modal-script.js
@@ -1,7 +1,6 @@
 const menu_modal = document.querySelector(".modal");
 
 const menu_button = document.querySelector(".menu-button");
-
 const side_menu_button=document.querySelector(".sidemenue-button")
 
 const menu_modal_close = document.querySelector(".modal-close");
@@ -17,6 +16,7 @@ function open_modal() {
 function close_modal() {
     menu_modal.style.display = 'none';
     page_content.style.overflowY = ''; 
+
 }
 
 menu_button.addEventListener("click", open_modal);
@@ -24,4 +24,3 @@ menu_button.addEventListener("click", open_modal);
 menu_modal_close.addEventListener('click', close_modal);
 
 side_menu_button.addEventListener('click',open_modal)
-
